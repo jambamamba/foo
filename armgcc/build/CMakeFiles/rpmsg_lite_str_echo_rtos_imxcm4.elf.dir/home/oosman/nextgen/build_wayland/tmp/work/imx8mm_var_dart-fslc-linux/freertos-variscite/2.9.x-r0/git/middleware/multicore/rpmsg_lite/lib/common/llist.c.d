fd2a88a79beba59c594cb7405f7f0b8dc6c1a0fc
oosman <osaeed@stenograph.com>
2021-10-15 12:02:37 -0500

    remoing output files

--- armgcc/build/CMakeFiles/rpmsg_lite_str_echo_rtos_imxcm4.elf.dir/home/oosman/nextgen/build_wayland/tmp/work/imx8mm_var_dart-fslc-linux/freertos-variscite/2.9.x-r0/git/middleware/multicore/rpmsg_lite/lib/common/llist.c.d
+++
@@ -1,5 +0,0 @@
-CMakeFiles/rpmsg_lite_str_echo_rtos_imxcm4.elf.dir/home/oosman/nextgen/build_wayland/tmp/work/imx8mm_var_dart-fslc-linux/freertos-variscite/2.9.x-r0/git/middleware/multicore/rpmsg_lite/lib/common/llist.c.obj: \
- /home/oosman/nextgen/build_wayland/tmp/work/imx8mm_var_dart-fslc-linux/freertos-variscite/2.9.x-r0/git/middleware/multicore/rpmsg_lite/lib/common/llist.c \
- /home/oosman/nextgen/build_wayland/tmp/work/imx8mm_var_dart-fslc-linux/freertos-variscite/2.9.x-r0/git/middleware/multicore/rpmsg_lite/lib/include/llist.h
-
-/home/oosman/nextgen/build_wayland/tmp/work/imx8mm_var_dart-fslc-linux/freertos-variscite/2.9.x-r0/git/middleware/multicore/rpmsg_lite/lib/include/llist.h:
